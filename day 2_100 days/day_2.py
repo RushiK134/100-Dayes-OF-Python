faa397e6873d29e85fe87d35402ca5e9fe9d7d59
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-12 23:45:41 +0530

    Add files via upload

---
+++ day 2_100 days/day_2.py
@@ -0,0 +1,10 @@
+name = input("What's your name?: ")
+food = input("What's your gavourit food: ")
+music = input("What's your favourite music?: ")
+city  = input("Where do you live?: ")
+print("You are "+name)
+print("You're probably hungry for "+ food)
+print("and you're definitely getting your groove on to "+ music)
+print("living in the amazing city "+city)
+
+#replit100DaysOfCode
