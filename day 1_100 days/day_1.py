2ba28257e004328c93fd736fd7566709fc0bf50f
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-12 23:35:20 +0530

    Add files via upload

---
+++ day 1_100 days/day_1.py
@@ -0,0 +1,9 @@
+print("Rushikesh")
+print("September 10th 2023")
+print("I am signing up for Replit's 100 days of Python challange!")
+print("I will make it sure to spend some time every day coding along, for a minimum of 10 minits a day.")
+print("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses ffor the coding from the middle of a field!")
+print("I am feeling 🥳😘")
+print("You can follow my progress at replit.com/@Rushik134")
+
+#replit100DaysOfCode
