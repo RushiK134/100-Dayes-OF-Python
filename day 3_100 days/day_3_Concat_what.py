af5a0d10f54bd387c857e00e085097090edec893
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-13 07:44:13 +0530

    Add files via upload
    Concat....What?
    Learn about Concatenating
    complete a wacky recipe maker project

---
+++ day 3_100 days/day_3_Concat_what.py
@@ -0,0 +1,19 @@
+print("Recipe Generator")
+
+print("")
+print("")
+
+food = input("Name a food > ")  # Mac & Cheese
+typePlant = input("Name a type of plant > ")  # Cactus
+cooking_method = input("Name a method of cooking > ")  # Saute
+wDburnedFood = input("What word describes burned food? > ")  # Ruined
+DIYitem = input("Name a DIY item > ")  # Hammers
+
+print("")
+print("")
+print("")
+
+print("Menu")
+print("Saute",food,"with",wDburnedFood,typePlant,"on a bed of",DIYitem)
+
+#replit100DaysOfCode
