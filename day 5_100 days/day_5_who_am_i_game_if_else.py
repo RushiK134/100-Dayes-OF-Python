5ce3db4fe8b55e44fd1ed12d9f172b0b5c9324a7
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-15 17:28:24 +0530

    Add files via upload
    Generate Actual Q&A's With 'if' Statements, "Create a Who Am I?" Game With Your Favorite TV/Movie Characters.

---
+++ day 5_100 days/day_5_who_am_i_game_if_else.py
@@ -0,0 +1,55 @@
+# myName = input("What is your name?: ")
+# if myName == "David" :
+#   print("Welcome Dude!")
+#   print("you're just the baldest dude I've ever seen")
+# elif myName == "Rushikesh" :
+#   print("Hey Rushikesh!")
+#   print("You've got a unique name!")
+# else:
+#   print("Who on earth are you?!")
+
+# print("")
+# print("")
+# print("")
+
+# catsOrDogs = input("Are you a cat person? or a dog person?: ")
+# if catsOrDogs == "cat":
+#   print("Meow")
+# else:
+#   print("Woof")
+
+# print("")
+# print("")
+#Challange
+print("Which character are you game")
+print("")
+
+print("Marvel Movie Character Creator")
+print("")
+
+hero1 = input("Do you like 'hanging around'?: ")
+if hero1 == "no":
+    print("Then you're not Spider-man")
+    print("he man i am looking for it...")
+else:
+    print("hey amazing you are Spider-man!")
+print(" ")
+
+hero2 = input("Do you have a 'gravelly' voice?: ")
+if hero2 == "no":
+    print("then you're not korg")
+else:
+    print("you are korg my boy")
+print("")
+
+hero3 = input("Do you often feel 'Marvelous'?: ")
+if hero3 == "yes":
+    print("Aha! You're Captain Marvel! Hi!")
+else:
+    print("You'r so unlucky...")
+    print("buddy sorry, you'r not matching with onyone of charactors")
+
+# replit100DaysOfCode
+
+
+#replit100DaysOfCode
