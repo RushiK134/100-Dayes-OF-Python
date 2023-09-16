06f9035d80fe5b6c35d1634fe154270151e844c4
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-16 07:48:51 +0530

    Add files via upload
    Day_6 
    What the elif is this!
    Learn elif statements. Created a login system to keep password safe from hackers.
    #replit100DaysOfCode

---
+++ day 6_100 days/day_6_Secure_login_system.py
@@ -0,0 +1,59 @@
+# print("SECURE LOGIN")
+# username = input("Username > ")
+# if username == "mark":
+#   print("Welcome Mark!")
+# elif username == "Suzanne":
+#   print("Hey there Suzanne!")
+# elif username == "rushikehs":
+#   print("Yo Rushi!")
+# else:
+#   print("Who are you go away!")
+
+# print(" ")
+# print(" ")
+
+# print("SECURE LOGIN")
+# username = input("Username > ")
+# password = input("Password > ")
+# if username == "mark" and password == "password":
+#   print("Welcome Mark!")
+# elif username == "Suzanne" and password == "Su21nne":
+#   print("Hey there Suzanne!")
+# elif username == "rushikehs" and password == "Rushi":
+#   print("Yo Rushi!")
+# else:
+#   print("Who are you go away!")
+
+#task
+
+print("SECURE LOGIN")
+
+print("")
+
+print("mark > password")
+print("Suzanne > Su21nne")
+print("rushikesh > Rushi")
+print("om > om123")
+print("khushi > password")
+print("mark > khushi123")
+
+print("")
+
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "mark" and password == "password":
+  print("Welcome back, Mark! We're glad to see you again.")
+elif username == "Suzanne" and password == "Su21nne":
+  print("Greetings, Suzanne! You've successfully logged in.")
+elif username == "rushikesh" and password == "Rushi":
+  print("Hi there, Rushikesh! You're now connected.")
+elif username == "om" and password == "om123":
+  print("Hey OM, you're in! Let's get started.")
+elif username == "khushi" and password == "khushi123":
+  print("Congratulations, Khushi you have successfully logged in.")
+else:
+  print("Invalid username or password. Please try again.")
+
+
+#replit100DaysOfCode
