3a6f1ccecc6c1c9583b9a6cc41f07130dec3d83a
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-18 07:40:44 +0530

    Add files via upload
    DAY 8 - Affirmation Generator
    Project day! Show the world some love with an affirmation generator. Send good vibes each day with a personalized message.
    #replit100DaysOfCode

---
+++ day 8_100 days/day_8_Affirmation_Generator.py
@@ -0,0 +1,84 @@
+# DAY 8 - Affirmation Generator
+# Project day! Show the world some love with an affirmation generator. Send good vibes each day with a personalized message.
+
+
+# DAY 8 -  Custome Affirmation Generator or Custome Insult Generator
+print("Custome Affirmation Generator")
+print("")
+
+name = input("What is your name: ")
+print("Hi", name, "!")
+
+print("")
+
+print("Please give me the answers of few of this questions")
+
+# sport = input("Favorite type of exercise or sport.: ")
+breakfast = input("Favorite breakfast.: ")
+# place = input("Favorite place to go out with friends.: ")
+music = input("Favorite Music.: ")
+# motivational_quote =input("Favorite motivational quote.: ")
+sweet = input("Favorite Sweet.: ")
+joy = input("What brings you joy on Sundays?: ")
+food = input("Favorite Food.: ")
+part_of_house = input("Favorite part of your house.: ")
+god = input("Favorite God.: ")
+
+print("")
+
+day_of_week = input("what day of the week is today?: ")
+
+print("")
+
+if day_of_week == "Monday" or day_of_week == "monday":
+    print("Hello,", name,
+          "! It's Monday, the start of a new week. Focus on the positive and embrace the opportunities ahead.")
+
+elif day_of_week == "Tuesday" or day_of_week == "tuesday":
+    print("Hello,", name, "! Happy Wednesday! Stay focused on your goals, and don't forget to enjoy", breakfast, ".")
+
+elif day_of_week == "Wednesday" or day_of_week == "wednesday":
+    print("Hello,", name, "! Happy Wednesday! Stay focused on your goals, and don't forget to enjoy", music, ".")
+
+elif day_of_week == "Thursday" or day_of_week == "thursday":
+    print("Hi,", name, "! It's Thursday, almost the weekend. Embrace the day with positivity, and savor", food, ".")
+
+elif day_of_week == "Friday" or day_of_week == "friday":
+    print("Hello,", name, "! TGIF! You've worked hard this week. Treat yourself to some", sweet, "as a reward.")
+
+elif day_of_week == "Saturday" or day_of_week == "saturday":
+    print("Hi,", name, "! It's Saturday, a day to relax and enjoy life. Spend time with loved ones and relish in",
+          part_of_house, ".")
+
+elif day_of_week == "Sunday" or day_of_week == "sunday":
+    print("Hello,", name, "! It's Sunday, a day of reflection. Be grateful for", god, "and the week ahead.")
+
+else:
+    print("Hey,", name, "! I'm not sure which day it is, but I hope you have a fantastic day filled with", joy, ".")
+
+print("")
+
+print(name, "Thanks for bring with us...🥰🥰")
+
+# output
+'''Custome Affirmation Generator
+
+What is your name: Rushikesh
+Hi Rushikesh !
+
+Please give me the answers of few of this questions
+Favorite breakfast.: sandwitch
+Favorite Music.: dhum-machale
+Favorite Sweet.: gulabjamun
+What brings you joy on Sundays?: relaxing
+Favorite Food.: kachori
+Favorite part of your house.: bed
+Favorite God.: Lord Ganesh
+
+what day of the week is today?: sunday
+
+Hello, Rushikesh ! It's Sunday, a day of reflection. Be grateful for Lord Ganesh and the week ahead.
+Rushikesh Thanks for bring with us...🥰🥰'''
+
+
+#replit100DaysOfCode
