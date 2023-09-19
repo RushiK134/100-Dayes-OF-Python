08cc25105b66e4625c84542442df89725cbd321b
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-19 12:25:51 +0530

    Add files via upload
     Felt like I was at Hogwarts with all the casting I did today 🏰🧙 ! Day 9 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day 9_100 days/day_9_Casting_Spells_on_Your_Code.py
@@ -0,0 +1,73 @@
+# day_9 Casting Spells on Your Code
+# Teach the computer to recognize numbers (and do the math) with the 'magic' of `int` and `float`!
+
+
+
+print("casting")
+
+# print("")
+
+# myScore = int(input("your score: "))
+# if myScore > 100000:
+#   print("Winner!")
+# else:
+#   print("Try again😭")
+
+#____________________________________________________
+
+# print("")
+
+# myScore = float(input("your score: "))
+# if myScore > 100000:
+#   print("Winner!")
+# else:
+#   print("Try again😭")
+
+#____________________________________________________
+
+print("Generation Identifier")
+
+print("")
+
+birthYear = int(input("What were you born? or Year of Bitrh.: "))
+if birthYear < 1883:
+  print("You are from very very Old Generation.")
+
+elif birthYear >= 1883 and birthYear <= 1900:
+  print("Hey!, You are from Lost Generation Man.")
+
+elif birthYear >= 1901 and birthYear <= 1927:
+  print("Hey!, You are from Greatest Generation Man.")
+
+elif birthYear >= 1928 and birthYear <= 1945:
+  print("Hey!, You are from Silent Generation Man.")
+
+elif birthYear >= 1946 and birthYear <= 1964:
+  print("Hey, Baby Boomers! How you doing.")
+
+elif birthYear >= 1965 and birthYear <= 1980:
+  print("Gen X! What's up?")
+
+elif birthYear >= 1981 and birthYear <= 1996:
+  print("Millenians! The age of tech!")
+
+elif birthYear >= 1967 and birthYear <= 2012:
+  print("Hey, Gen Z! TikTok much?")
+
+elif birthYear > 2013 and birthYear <= 2025:
+  print("Generation Alpha! Booming Man...")
+
+# elif birthYear > 2025:
+#   print("Sorry! currently not have Generation defined.")
+
+else:
+  print("Try again!")
+
+
+
+
+
+
+#replit100DaysOfCode
+
+# Felt like I was at Hogwarts with all the casting I did today 🏰🧙 ! Day 9 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
