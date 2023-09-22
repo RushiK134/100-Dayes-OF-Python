937926b62c53a03ac28a0c66f762536c10822340
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-22 08:14:47 +0530

    Add files via upload
    Smashed some gnarly bugs today 🐛 .  Day 12 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit #https://join.replit.com/python

---
+++ day 12_100 days/day_12_debug_code.py
@@ -0,0 +1,61 @@
+# day_12 Find All the Bugs!
+# This course has taught you to squash bugs like a true programmer. Find the errors and start debugging like a pro!
+
+# Smashed some gnarly bugs today 🐛 .  Day 12 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit
+# https://join.replit.com/python?
+
+print("Debug 🚫🪲 my code.")
+print("👉 Day 12 Challenge")
+print("👉 Try and fix this code which is full of errors.")
+print(
+    "First, delete any other code in your main.py file. Copy each code snippet below into main.py by clicking the "
+    "copy icon in the top right of each code box. Then, hit run and see what errors occur. Fix the errors and press "
+    "run again until you are error free. Click on the 👀 Answer to compare your code to the correct code.")
+
+print("")
+print("")
+
+'''
+print("100 Days of Code QUIZ")
+print()
+print("How many can you answer correctly?)
+ans1 = ("What language are we writing in?")
+if ans1 == "python":
+  print("Correct")
+else:
+  print("Nope🙈
+ans2 = input("Which lesson number is this?")
+if(ans2>12):
+print("We're not quite that far yet")
+else:
+  print("We've gone well past that!")
+elif(ans2==12):
+  print("That's right!")
+  '''
+
+print("100 Days of Code QUIZ")
+print("")
+
+print("How many can you answer correctly?")
+
+print("")
+
+ans1 = input("What language are we writing in?: ")
+
+if ans1 == "python" or ans1 == "Python":
+    print("Correct")
+else:
+    print("Nope🙈")
+
+print("")
+
+ans2 = int(input("Which lesson number is this?: "))
+
+if ans2 > 12:
+    print("We're not quite that far yet")
+elif ans2 == 12:
+    print("That's right!")
+else:
+    print("We've gone well past that!")
+
+# replit100DaysOfCode
