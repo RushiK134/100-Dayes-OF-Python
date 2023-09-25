33b6cae4edbbc776107a94db0476d42e419a360e
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-25 07:34:18 +0530

    Add files via upload
    What does the 🦊 say ? Find out with my custom animal sound generator (just like Alexa)! Day 15 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day 15_100 days/day_15_while_loop.py
@@ -0,0 +1,151 @@
+# All About the Loop
+# Make your games much more epic by adding a loop to repeat the game over and over...
+
+
+print("All About Loops")
+print("While Loop")
+print("A while loop allows your code to repeat itself based on a condition you set.")
+
+print("")
+print("")
+
+# print("Counter up to 10")
+# counter = 0
+# while counter < 10:
+#   print(counter)
+#   counter +=1
+
+# print("")
+
+# ______________________________________________________________________
+
+# print("Counter up to 100")
+# counter = 0
+# while counter < 100:
+#   print(counter)
+#   counter +=1
+
+# print("")
+
+# ___________________________________________________________________
+
+
+# exit = ""
+# while exit != "yes":
+#   print("🥳")
+#   exit = input("exit?: ")
+
+# ___________________________________________________________________
+
+# print("fix the broken code")
+# print("")
+# print("Can you extend this code to print to 100?")
+
+'''counter = 0
+while counter < 25:
+  print(counter)'''
+
+# counter = 0
+# while counter < 25:
+#   print(counter)
+#   counter += 1
+
+
+'''counter = 0
+while counter >= 12:
+  print(counter)
+  counter += 1'''
+
+# counter = 0
+# while counter <= 12:
+#   print(counter)
+#   counter += 1
+
+
+'''exit = ""
+while exit = "yes":
+  print("🥳")
+exit = input("Exit?: ")'''
+
+# exit = ""
+# while exit != "yes":
+#   print("🥳")
+#   exit = input("Exit?: ")
+
+
+# ________________________________________________________________
+
+print("👉 Day 15 Challenge")
+print("")
+
+print("Write a program that loops. Inside the loop, ask the user what animal sound they want to hear.")
+
+print("")
+
+print("List  of animals")
+print("Cow")
+print("Dog")
+print("Cat")
+print("Dolphin")
+print("Horse")
+print("Elephant")
+
+print("")
+
+exit = "no"
+
+while exit == "no" or exit == "No":
+    animalName = input("What animal do you want?:")
+    if animalName == "cow" or animalName == "Cow":
+        print("A cow goes moo.")
+
+    elif animalName == "dog" or animalName == "Dog":
+        print("A dog barks.")
+
+    elif animalName == "cat" or animalName == "Cat":
+        print("A cat meows.")
+
+    elif animalName == "elephant" or animalName == "Elephant":
+        print("An elephant trumpets.")
+
+    elif animalName == "horse" or animalName == "Horse":
+        print("A horse neighs.")
+
+    elif animalName == "dolphin" or animalName == "Dolphin":
+        print("A dolphin clicks and whistles.")
+
+    else:
+        print("I don't know that animal")
+
+    exit = input("Do you wnat to exit?: ")
+
+# ____________________________________________________________________________
+
+
+# exit = "no"
+
+
+# while exit == "no":
+#   animal_sound = input("What animal sound do you want to hear?")
+
+#   if animal_sound == "cow" or animal_sound == "Cow":
+#     print("🐮 Moo")
+#   elif animal_sound == "pig" or animal_sound == "Pig":
+#     print ("🐷 Oink")
+#   elif animal_sound == "sheep" or animal_sound == "Sheep":
+#     print ("🐑 Baaa")
+#   elif animal_sound == "duck" or animal_sound == "Duck":
+#     print("🦆 Quack")
+#   elif animal_sound == "dog" or animal_sound == "Dog":
+#     print("🐶 Woof")
+#   elif animal_sound == "cat" or animal_sound == "Cat":
+#     print("🐱 Meow")
+#   else:
+#     print("I don't know that animal sound. Try again.")
+
+
+#   exit = input("Do you want to exit?: ")
+
+# What does the 🦊 say ? Find out with my custom animal sound generator (just like Alexa)! Day 15 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+
+#replit100DaysOfCode
