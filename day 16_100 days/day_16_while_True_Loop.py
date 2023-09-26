1b55e70ea567087787bd8c3d6c12ede47cadba7a
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-26 07:38:53 +0530

    Add files via upload
    Can you guess the lyrics to my favorite song 🎶 🎤?  Day 16 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day 16_100 days/day_16_while_True_Loop.py
@@ -0,0 +1,160 @@
+# Make it Stop!
+# Whoops. You made an accidental infinite loop...and nightmare. Introducing..."while True"...to make it stop.
+
+
+
+# Can you guess the lyrics to my favorite song 🎶 🎤?  Day 16 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+
+
+
+print("while True Loop")
+print("")
+
+# while True:
+#   print("This program is running")
+#   goAgain = input("Go again?: ")
+
+#   if goAgain == "no":
+#     break
+# print('Aww, I was having a good time 😭')
+
+
+# _______________________________________________________________________________
+
+# print("Fix Common Errors")
+# print("")
+
+'''counter = 0
+while true:
+  answer = int(input("Enter a number: "))
+  print("Adding it up!")
+  counter += answer
+  print("Current total is", counter)
+  addAnother = input("Add another? ")
+  if addAnother == "no":
+    break
+print("Bye!")'''
+
+# print("Simple calculator that adding numbers")
+# counter = 0
+# while True:
+#   answer = int(input("Enter a number: "))
+#   print("Adding it up!")
+#   counter += answer
+#   print("Current total is", counter)
+#   addAnother = input("Add another? ")
+#   if addAnother == "no":
+#     break
+# print("Bye!")
+
+# _____________________________________
+# print("")
+
+'''counter = 0
+while True:
+  answer = int(input("Enter a number: "))
+  print("Adding it up!")
+  counter += answer
+  print("Current total is", counter)
+addAnother = input("Add another? ")
+if addAnother == "no":
+  break
+print("Bye!")'''
+
+# print("")
+
+# counter = 0
+# while True:
+#   answer = int(input("Enter a number: "))
+#   print("Adding it up!")
+#   counter += answer
+#   print("Current total is", counter)
+#   addAnother = input("Add another? ")
+#   if addAnother == "no":
+#     break
+# print("Bye!")
+
+# print("")
+# ______________________________________
+'''while true:
+  color = input("Enter a color: ")
+  if color = "red":
+  break
+  else:
+    print("Cool color!")
+print("I don't like red")'''
+
+# while True:
+#   color = input("Enter a color: ")
+#   if color == "red":
+#     break
+#   else:
+#     print("Cool color!")
+# print("I don't like red")
+
+
+# _________________________________________________________________________
+
+# print("Day 16 Challange")
+
+# print("")
+
+# print("Name the Lyrics game")
+
+# print("")
+
+# print("Fill in the blank lyrics!")
+# print("(Type in the blank lyrics and see if you are as cool as me.)")
+
+# print("")
+
+# count = 0
+# while True:
+#   print("Never going to ______ you up.")
+#   userInput = input(":- ")
+#   if userInput == "give" :
+#     break
+
+#   else:
+#     print("Nope, try again.")
+#     count += 1
+
+# print("Well done! It only took you",count," attempts.")
+
+
+# replit100DaysOfCode"
+
+'''Tune maari entriyaan re
+
+Dil mein baji ghantiyaan re
+
+Tang tang tang...
+'''
+
+print("Day 16 Challange")
+
+print("")
+
+print("Name the Lyrics game")
+
+print("")
+
+print("Fill in the blank lyrics!")
+print("(Type in the blank lyrics and see if you are as cool as me.)")
+
+print("")
+
+count = 0
+while True:
+    print("Tune maari entriyaan re Dil mein baji ghantiyaan re Tang ______ tang...")
+    userInput = input(":- ")
+    if userInput == "tang":
+        break
+
+    else:
+        print("Nope, try again.")
+        count += 1
+
+print("Well done! It only took you", count, " attempts.")
+
+#replit100DaysOfCode
