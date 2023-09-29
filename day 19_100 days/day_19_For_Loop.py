64662981748309064da09a8fd089f107b05fad7c
RushiK134 <117682105+RushiK134@users.noreply.github.com>
2023-09-29 16:32:54 +0530

    Add files via upload
    Built an interest calculator to see how much money I would owe! A bit boring, I know, but necessary for #reallife. Day 19 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day 19_100 days/day_19_For_Loop.py
@@ -0,0 +1,68 @@
+# Let's Be a Bit Lazy!
+# What if you do have an idea of how often you want something to repeat (or you just want to be a bit lazy)? Introducing...the `for` loop.
+
+# Built an interest calculator to see how much money I would owe! A bit boring, I know, but necessary for #reallife. Day 19 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
+
+
+print("For Loop")
+print("")
+
+# counter = 0
+# while counter < 10:
+#   print(counter)
+#   counter += 1
+
+# for i in range(10):
+#   print(i)
+# print("")
+
+# print("Common Errors")
+
+'''total = 0
+for number in range 100:
+  total += number
+  print(total)'''
+
+# total = 0
+# for number in range (100):
+#   total += number
+#   print(total)
+
+# print("")
+
+'''for days in range(7):
+  print("Day", day)'''
+
+# for days in range(7):
+#   print("Day", days)
+
+# print("")
+
+print("Fix My Code")
+
+'''total = 10
+while counter in range 100:
+  total += 10
+  print("total")'''
+
+# total = 10
+# for counter in range (100):
+#   total += 10
+#   print("total")
+
+# print("")
+
+print("👉 Day 19 Challenge")
+
+print("")
+
+loan = 1000
+for i in range(10):
+    loan += loan * (5 / 100)
+    print("Year", i + 1, "is", round(loan, 2))
+
+
+
+
+
+#replit100DaysOfCode
